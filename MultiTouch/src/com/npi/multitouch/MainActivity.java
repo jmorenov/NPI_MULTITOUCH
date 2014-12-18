63dc7ee8a7493d9134ef946a5da9b1130cf4d0b5
Jmorenov <jmorenov28@gmail.com>
2014-12-18 01:07:12 +0100

    Clase Touch añadida.

--- MultiTouch/src/com/npi/multitouch/MainActivity.java
+++ MultiTouch/src/com/npi/multitouch/MainActivity.java
@@ -1,5 +1,7 @@
 package com.npi.multitouch;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -11,8 +13,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
-    private String DEBUG_TAG = "DEBUG: ";
-
+    private ArrayList<Touch> touchEvents;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,71 +28,10 @@ public class MainActivity extends Activity
 	testMultiTouch();
 	super.onStart();
     }
-
-    // Given an action int, returns a string description
-    public static String actionToString(int action)
-    {
-	switch (action)
-	{
-
-	case MotionEvent.ACTION_DOWN:
-	    return "Down";
-	case MotionEvent.ACTION_MOVE:
-	    return "Move";
-	case MotionEvent.ACTION_POINTER_DOWN:
-	    return "Pointer Down";
-	case MotionEvent.ACTION_UP:
-	    return "Up";
-	case MotionEvent.ACTION_POINTER_UP:
-	    return "Pointer Up";
-	case MotionEvent.ACTION_OUTSIDE:
-	    return "Outside";
-	case MotionEvent.ACTION_CANCEL:
-	    return "Cancel";
-	}
-	return "";
-    }
-
-    private int mActivePointerId;
-
+    
     public boolean onTouchEvent(MotionEvent event)
     {
-	/*// Get the pointer ID
-	mActivePointerId = event.getPointerId(0);
-
-	// ... Many touch events later...
-
-	// Use the pointer ID to find the index of the active pointer
-	// and fetch its position
-	int pointerIndex = event.findPointerIndex(mActivePointerId);
-	// Get the pointer's current position
-	float x = event.getX(pointerIndex);
-	float y = event.getY(pointerIndex);*/
 	
-	int action = MotionEventCompat.getActionMasked(event);
-	// Get the index of the pointer associated with the action.
-	int index = MotionEventCompat.getActionIndex(event);
-	int xPos = -1;
-	int yPos = -1;
-
-	//Log.d(DEBUG_TAG, "The action is " + actionToString(action));
-
-	if (event.getPointerCount() > 1)
-	{
-	    Log.d(DEBUG_TAG, "Multitouch event");
-	    // The coordinates of the current screen contact, relative to
-	    // the responding View or Activity.
-	    xPos = (int) MotionEventCompat.getX(event, index);
-	    yPos = (int) MotionEventCompat.getY(event, index);
-
-	} else
-	{
-	    // Single touch event
-	    Log.d(DEBUG_TAG, "Single touch event");
-	    xPos = (int) MotionEventCompat.getX(event, index);
-	    yPos = (int) MotionEventCompat.getY(event, index);
-	}
-
 	return false;
     }
 
