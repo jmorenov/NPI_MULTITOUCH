065ac2c080f121223338888b83a3b786d206120d
Jmorenov <jmorenov28@gmail.com>
2014-12-19 07:03:04 +0100

    Añadidos comentarios JavaDoc.

--- MultiTouch/src/com/npi/multitouch/MainActivity.java
+++ MultiTouch/src/com/npi/multitouch/MainActivity.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2014 Javier Moreno, Alberto Quesada
+ * 
+ * This file is part of MultiTouch.
+ * 
+ * MultiTouch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MultiTouch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MultitTouch.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.npi.multitouch;
 
 import android.app.Activity;
@@ -7,38 +25,67 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 /**
- * @author Javier Moreno
- * @author Alberto Quesada
- * @version %I%, %G%
- * @since 1.0
+ * Clase MainActivity. Controla la ejecución de la app, llamando a la creación
+ * de la interfaz.
+ * 
+ * @author Francisco Javier Moreno Vega
+ * @author Alberto Quesada Aranda
+ * @version 19.12.2014
+ * @since 17.12.2014
  */
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    /**
+     * Called when the activity is starting. This is where most initialization
+     * should go: calling setContentView(int) to inflate the activity's UI,
+     * using findViewById(int) to programmatically interact with widgets in the
+     * UI, calling managedQuery(android.net.Uri, String[], String, String[],
+     * String) to retrieve cursors for data being displayed, etc.
+     * <p>
+     * You can call finish() from within this function, in which case
+     * onDestroy() will be immediately called without any of the rest of the
+     * activity lifecycle (onStart(), onResume(), onPause(), etc) executing.
+     *
+     * @param savedInstanceState
+     *            If the activity is being re-initialized after previously being
+     *            shut down then this Bundle contains the data it most recently
+     *            supplied in onSaveInstanceState(Bundle).
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+    }
 
-	@Override
-	protected void onStart() {
-		testMultiTouch();
-		super.onStart();
-	}
+    /**
+     * Called after onCreate(Bundle) — or after onRestart() when the activity
+     * had been stopped, but is now again being displayed to the user. It will
+     * be followed by onResume().
+     * <p>
+     * LLama al método que comprueba si el dispositivo soporta múltiples puntos
+     * de detección en la pantalla.
+     * 
+     * @see testMultiTouch()
+     */
+    @Override
+    protected void onStart() {
+	testMultiTouch();
+	super.onStart();
+    }
 
-	private void testMultiTouch() {
-		Context context = getApplicationContext();
-		CharSequence text;
-		if (hasMultitouch())
-			text = "MultiTouch funciona en este dispositivo.";
-		else
-			text = "MultiTouch no funciona en este dispositivo.";
-		Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
-		toast.show();
-	}
-
-	private boolean hasMultitouch() {
-		return getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_TOUCHSCREEN_MULTITOUCH);
-	}
+    /**
+     * Comprueba si el dispositivo soporta múltiples puntos de detección en la
+     * pantalla.
+     */
+    private void testMultiTouch() {
+	Context context = getApplicationContext();
+	CharSequence text;
+	if (getPackageManager().hasSystemFeature(
+		PackageManager.FEATURE_TOUCHSCREEN_MULTITOUCH))
+	    text = "MultiTouch funciona en este dispositivo.";
+	else
+	    text = "MultiTouch no funciona en este dispositivo.";
+	Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+	toast.show();
+    }
 }
--- MultiTouch/src/com/npi/multitouch/TouchView.java
+++ MultiTouch/src/com/npi/multitouch/TouchView.java
@@ -1,5 +1,22 @@
+/**
+ * Copyright 2014 Javier Moreno, Alberto Quesada
+ * 
+ * This file is part of MultiTouch.
+ * 
+ * MultiTouch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MultiTouch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MultitTouch.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.npi.multitouch;
-
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -12,115 +29,161 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * @author Javier Moreno
- * @author Alberto Quesada
- * @version %I%, %G%
- * @since 1.0
+ * Clase TouchView. Controla la interfaz de la app y los puntos de contacto
+ * detectados en la pantalla.
+ * 
+ * @author Francisco Javier Moreno Vega
+ * @author Alberto Quesada Aranda
+ * @version 19.12.2014
+ * @since 17.12.2014
  */
 public class TouchView extends View {
 
-	private static final int SIZE = 30;
+    /**
+     * Radio de la circunferencia que se pinta en el punto de contacto
+     * detectado.
+     */
+    private static final int SIZE = 30;
 
-	private Paint mPaint;
-	private Paint paint;
+    /**
+     * Objetos Paint que controlan el estilo de pintar el recorrido y las
+     * circunferencias.
+     */
+    private Paint mPaint;
+    private Paint paint;
 
-	private SparseArray<PointF> mActivePointers;
-	private SparseArray<Path> paths;
+    /**
+     * Estructuras de datos SparseArray donde almacenar los puntos de contacto
+     * detectados y el recorrido en la pantalla.
+     */
+    private SparseArray<PointF> mActivePointers;
+    private SparseArray<Path> paths;
 
-	public TouchView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		initView();
+    /**
+     * Constructor de la clase TouchView, inicia la vista.
+     * 
+     * @param context
+     * @param attrs
+     * 
+     * @see initView()
+     */
+    public TouchView(Context context, AttributeSet attrs) {
+	super(context, attrs);
+	initView();
+    }
+
+    /**
+     * Inicia la vista que usaremos para la interfaz.
+     */
+    private void initView() {
+	mActivePointers = new SparseArray<PointF>();
+	paths = new SparseArray<Path>();
+
+	mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	mPaint.setStyle(Paint.Style.STROKE);
+
+	paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	paint.setStrokeWidth(6f);
+	paint.setStyle(Paint.Style.STROKE);
+	paint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    /**
+     * Call this view's OnClickListener, if it is defined. Performs all normal
+     * actions associated with clicking: reporting accessibility event, playing
+     * a sound, etc.
+     * 
+     * @return True there was an assigned OnClickListener that was called, false
+     *         otherwise is returned.
+     */
+    @Override
+    public boolean performClick() {
+	// Calls the super implementation, which generates an AccessibilityEvent
+	// and calls the onClick() listener on the view, if any
+	super.performClick();
+	// Handle the action for the custom click here
+
+	return true;
+    }
+
+    /**
+     * Implement this method to handle touch screen motion events.
+     * 
+     * If this method is used to detect click actions, it is recommended that
+     * the actions be performed by implementing and calling performClick(). This
+     * will ensure consistent system behavior, including:
+     * 
+     * - obeying click sound preferences - dispatching OnClickListener calls -
+     * handling ACTION_CLICK when accessibility features are enabled
+     * 
+     * @param event
+     *            The motion event
+     * 
+     * @return True if the event was handled, false otherwise.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+	int pointerIndex = event.getActionIndex();
+	int pointerId = event.getPointerId(pointerIndex);
+	int maskedAction = event.getActionMasked();
+
+	switch (maskedAction) {
+
+	case MotionEvent.ACTION_DOWN:
+	case MotionEvent.ACTION_POINTER_DOWN: {
+	    PointF point = new PointF();
+	    point.x = event.getX(pointerIndex);
+	    point.y = event.getY(pointerIndex);
+	    mActivePointers.put(pointerId, point);
+	    Path path = new Path();
+	    path.moveTo(point.x, point.y);
+	    paths.put(pointerId, path);
+	    break;
 	}
-
-	private void initView() {
-		mActivePointers = new SparseArray<PointF>();
-		paths = new SparseArray<Path>();
-
-		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mPaint.setStyle(Paint.Style.STROKE);
-
-		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		paint.setStrokeWidth(6f);
-		paint.setStyle(Paint.Style.STROKE);
-		paint.setStrokeJoin(Paint.Join.ROUND);
+	case MotionEvent.ACTION_MOVE: {
+	    for (int size = event.getPointerCount(), i = 0; i < size; i++) {
+		PointF point = mActivePointers.get(event.getPointerId(i));
+		Path path = paths.get(event.getPointerId(i));
+		if (point != null && path != null) {
+		    point.x = event.getX(i);
+		    point.y = event.getY(i);
+		    path.lineTo(point.x, point.y);
+		}
+	    }
+	    break;
 	}
-	
-	@Override
-	 public boolean performClick() {
-	  // Calls the super implementation, which generates an AccessibilityEvent
-	        // and calls the onClick() listener on the view, if any
-	        super.performClick();
-
-	        // Handle the action for the custom click here
-
-	        return true;
-	 }
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		// get pointer index from the event object
-		int pointerIndex = event.getActionIndex();
-
-		// get pointer ID
-		int pointerId = event.getPointerId(pointerIndex);
-
-		// get masked (not specific to a pointer) action
-		int maskedAction = event.getActionMasked();
-		
-		switch (maskedAction) {
-
-		case MotionEvent.ACTION_DOWN:
-		case MotionEvent.ACTION_POINTER_DOWN: {
-			// We have a new pointer. Lets add it to the list of pointers
-			PointF point = new PointF();
-			point.x = event.getX(pointerIndex);
-			point.y = event.getY(pointerIndex);
-			mActivePointers.put(pointerId, point);
-			Path path = new Path();
-			path.moveTo(point.x, point.y);
-			paths.put(pointerId, path);
-			break;
-		}
-		case MotionEvent.ACTION_MOVE: { // a pointer was moved
-			for (int size = event.getPointerCount(), i = 0; i < size; i++) {
-				PointF point = mActivePointers.get(event.getPointerId(i));
-				Path path = paths.get(event.getPointerId(i));
-				if (point != null && path != null) {
-					point.x = event.getX(i);
-					point.y = event.getY(i);
-					path.lineTo(point.x, point.y);
-				}
-			}
-			break;
-		}
-
-		case MotionEvent.ACTION_UP:
-		case MotionEvent.ACTION_POINTER_UP:
-		case MotionEvent.ACTION_CANCEL: {
-			mActivePointers.remove(pointerId);
-			paths.remove(pointerId);
-			performClick();
-			break;
-		}
-		}
-		invalidate();
-
-		return true;
+	case MotionEvent.ACTION_UP:
+	case MotionEvent.ACTION_POINTER_UP:
+	case MotionEvent.ACTION_CANCEL: {
+	    mActivePointers.remove(pointerId);
+	    paths.remove(pointerId);
+	    performClick();
+	    break;
 	}
-
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-
-		if (mActivePointers.size() > 1 && paths.size() > 1) {
-			// draw all pointers
-			for (int size = mActivePointers.size(), i = 0; i < size; i++) {
-				PointF point = mActivePointers.valueAt(i);
-				Path path = paths.valueAt(i);
-				canvas.drawCircle(point.x, point.y, SIZE, mPaint);
-				canvas.drawPath(path, paint);
-			}
-		}
 	}
+	invalidate();
+
+	return true;
+    }
+
+    /**
+     * Implement this to do your drawing.
+     * 
+     * @param canvas
+     *            the canvas on which the background will be drawn.
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+	super.onDraw(canvas);
+
+	if (mActivePointers.size() > 1 && paths.size() > 1) {
+	    for (int size = mActivePointers.size(), i = 0; i < size; i++) {
+		PointF point = mActivePointers.valueAt(i);
+		Path path = paths.valueAt(i);
+		canvas.drawCircle(point.x, point.y, SIZE, mPaint);
+		canvas.drawPath(path, paint);
+	    }
+	}
+    }
 
 }
